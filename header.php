eea67ec08e3dc088dadfe1e93727da712d724264
kotarot <kotarot@outlook.com>
2014-12-04 17:03:56 +0900

    Changed initial-scale.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-<meta name="viewport" content="width=device-width, initial-scale=0.8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php if (is_front_page()) : ?>
     <title>Top of <?php bloginfo('name'); ?></title>
 <?php else : ?>
