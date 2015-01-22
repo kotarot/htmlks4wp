5e565eca31341367c1bdb8925194d9f9edf48794
kotarot <kotarot@outlook.com>
2015-01-22 23:01:06 +0900

    New icons.

--- single.php
+++ single.php
@@ -16,10 +16,10 @@ while (have_posts()) : the_post(); ?>
 
 <h1><a class="post-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 <div class="post-meta attributes">
-	<span class="post-date"><i class="icon-calendar"></i> Posted on <?php echo get_the_date(); ?></span>,
-	<span class="post-category"><i class="icon-folder-close"></i> Category: <?php the_category(' '); ?></span>,
-	<span class="post-tags"><i class="icon-tags"></i> <?php the_tags('Tags: ', ' '); ?></span>,
-	<span class="post-comments"><i class="icon-comments"></i> <?php comments_popup_link('0 comments', '1 comment', '% comments'); ?></span>
+	<span class="post-date"><i class="fa fa-calendar"></i> Posted on <?php echo get_the_date(); ?></span>,
+	<span class="post-category"><i class="fa fa-folder"></i> Category: <?php the_category(' '); ?></span>,
+	<span class="post-tags"><i class="fa fa-tags"></i> <?php the_tags('Tags: ', ' '); ?></span>,
+	<span class="post-comments"><i class="fa fa-comments"></i> <?php comments_popup_link('0 comments', '1 comment', '% comments'); ?></span>
 </div><!-- /.post-meta .attributes -->
 <div class="col_12 content content-post">
 	<?php the_content(); ?>
