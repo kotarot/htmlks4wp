efd140c5f1ba3f333a91a94e529784b04d035097
Kotaro Terada <kotaro@tribox.jp>
2015-06-26 19:22:17 +0900

    Changed Japanese to English.

--- 404.php
+++ 404.php
@@ -3,9 +3,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <h2>Page Not Found (&gt; &lt;)</h2>
 <div class="col_12">
-	<p>アクセスしたページは削除されたかURLが変更されています．<br>
-	上部メニューまたは右上の検索フォームより目的のページをお探しください．</p>
-	<p><a href="<?php echo home_url('/'); ?>">トップページへ戻る</a></p>
+	<p>The page you access has been deleted.</p>
+	<p><a href="<?php echo home_url('/'); ?>">Back to Top</a></p>
 </div><!-- /.col_12 -->
 </div><!-- /#post-<?php the_ID(); ?> -->
 
