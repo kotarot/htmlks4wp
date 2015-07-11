eae9fa4ce3896081e16d34a8c389948b8be3d951
Kotaro Terada <kotaro@tribox.jp>
2015-07-12 03:08:35 +0900

    Hide menu when scrolling down.

--- footer.php
+++ footer.php
@@ -11,5 +11,21 @@ This website is built with <a href="http://www.wordpress.org/" target="_blank">W
 
 <?php wp_footer(); ?>
 
+<script>
+var menu_height = $('#navbar').height();
+var start_pos = 0;
+$(window).scroll(function() {
+    var current_pos = $(this).scrollTop();
+    if (current_pos > start_pos) {
+        if (200 <= $(window).scrollTop()) {
+            $('#navbar').css('top', '-' + menu_height + 'px');
+        }
+    } else {
+        $('#navbar').css('top', '0');
+    }
+    start_pos = current_pos;
+});
+</script>
+
 </body>
 </html>
