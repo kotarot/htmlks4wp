f853657abbdf9f44bdd3cc3b8addc022c3493f26
Kotaro Terada <kotaro@tribox.jp>
2015-10-15 21:04:56 +0900

    Remove an unnecessary close tag

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div class="clear"></div>
 <footer>
 &copy; Copyright <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a> All Rights Reserved.<br>
-Main author: <span class="vcard author"><span class="fn"><?php the_author(); ?></span></span></a>.
+Main author: <span class="vcard author"><span class="fn"><?php the_author(); ?></span></span>.
 WordPress theme: <a href="https://github.com/kotarot/htmlks4wp" target="_blank">htmlks4wp</a>.
 This website is built with <a href="http://www.wordpress.org/" target="_blank">WordPress</a> and
 <a href="https://github.com/kotarot/htmlks4wp" target="_blank">htmlks4wp</a> (based on <a href="http://www.99lime.com" target="_blank">HTML KickStart</a>).
