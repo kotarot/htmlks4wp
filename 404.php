bc350afae43af7658536e3bbb9a2bf283a6a17fa
Kotaro Terada <tera@toki.waseda.jp>
2015-11-20 00:48:32 +0900

    Apply i18n functions in 404.php

--- 404.php
+++ 404.php
@@ -3,8 +3,8 @@
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <h2>Page Not Found (&gt; &lt;)</h2>
 <div class="col_12">
-	<p>The page you access has been deleted.</p>
-	<p><a href="<?php echo home_url('/'); ?>">Back to Top</a></p>
+	<p><?php _e('The page you access has been deleted or moved.', 'htmlks4wp'); ?></p>
+	<p><a href="<?php echo home_url('/'); ?>"><?php _e('Back to Top', 'htmlks4wp'); ?></a></p>
 </div><!-- /.col_12 -->
 </div><!-- /#post-<?php the_ID(); ?> -->
 
